06544d77b7360892cc10aaa708d3775b5a66c372
pTinosq <tinosps@gmail.com>
2024-10-15 14:44:09 +0100

    Payment deletion functionality (frontend)

--- src/components/PaymentCard/index.tsx
+++ src/components/PaymentCard/index.tsx
@@ -1,5 +1,11 @@
-import React from "react";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React, { useState } from "react";
+import {
+	StyleSheet,
+	Text,
+	TouchableOpacity,
+	Vibration,
+	View,
+} from "react-native";
 import type { Payment } from "../../redux/slices/peopleSlice";
 import { formatDate } from "../../utils/date-utils";
 import { formatMoney } from "../../utils/payment-utils";
@@ -7,11 +13,15 @@ import { formatMoney } from "../../utils/payment-utils";
 const styles = StyleSheet.create({
 	container: {
 		borderRadius: 8,
+		width: "100%",
+		position: "relative", // Add relative position to the container
+	},
+	contentContainer: {
 		paddingVertical: 12,
 		paddingHorizontal: 16,
-		width: "100%",
 		borderColor: "#dddddd",
 		borderWidth: 2,
+		borderRadius: 8,
 	},
 	cardHeader: {
 		flexDirection: "row",
@@ -46,6 +56,23 @@ const styles = StyleSheet.create({
 		fontSize: 12,
 		color: "#333",
 	},
+	deleteOverlay: {
+		position: "absolute",
+		top: 0,
+		left: 0,
+		width: "100%",
+		height: "100%",
+		backgroundColor: "rgba(255, 0, 0, 0.7)",
+		justifyContent: "center",
+		alignItems: "center",
+		borderRadius: 8,
+		zIndex: 1,
+	},
+	deleteOverlayText: {
+		fontSize: 16,
+		color: "#fff",
+		fontWeight: "bold",
+	},
 });
 
 interface PaymentCardProps {
@@ -56,32 +83,57 @@ export default function PaymentCard(props: PaymentCardProps) {
 	const backgroundColor =
 		props.paymentData.type === "lent" ? "#eaffea" : "#ffeaea";
 
+	const [isDeleteMode, setIsDeleteMode] = useState(false);
+
 	function handleOnPress() {
-		// TODO: Do something?
+		if (isDeleteMode) {
+			setIsDeleteMode(false);
+		}
+	}
+
+	function handleOnHold() {
+		Vibration.vibrate(30);
+
+		if (isDeleteMode) {
+			console.log("473738", "DELETED");
+			return;
+		}
+
+		setIsDeleteMode(true);
 	}
 
 	return (
 		<TouchableOpacity
-			style={[styles.container, { backgroundColor }]}
+			style={[styles.container]}
 			onPress={handleOnPress}
+			onLongPress={handleOnHold}
 		>
-			<View style={styles.cardHeader}>
-				<Text style={styles.dateText}>
-					{formatDate(props.paymentData.date)}
-				</Text>
-				<Text
-					style={[
-						styles.totalMoney,
-						props.paymentData.type === "lent" ? styles.green : styles.red,
-					]}
-				>
-					{formatMoney(props.paymentData.amount)}
-				</Text>
+			<View style={[styles.contentContainer, { backgroundColor }]}>
+				<View style={styles.cardHeader}>
+					<Text style={styles.dateText}>
+						{formatDate(props.paymentData.date)}
+					</Text>
+					<Text
+						style={[
+							styles.totalMoney,
+							props.paymentData.type === "lent" ? styles.green : styles.red,
+						]}
+					>
+						{formatMoney(props.paymentData.amount)}
+					</Text>
+				</View>
+				<View style={styles.cardBody}>
+					<Text style={styles.title}>{props.paymentData.description}</Text>
+					<Text style={styles.bodyText}>{props.paymentData.type}</Text>
+				</View>
 			</View>
-			<View style={styles.cardBody}>
-				<Text style={styles.title}>{props.paymentData.description}</Text>
-				<Text style={styles.bodyText}>{props.paymentData.type}</Text>
-			</View>
+			{isDeleteMode && (
+				<View style={styles.deleteOverlay}>
+					<Text style={styles.deleteOverlayText}>
+						Tap to cancel | Hold to delete
+					</Text>
+				</View>
+			)}
 		</TouchableOpacity>
 	);
 }
